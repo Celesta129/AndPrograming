ca48b86bb0670396de67d67faf2c77fde54dcf93
Kim Ki Tae <celesta129@naver.com>
2020-06-16 09:25:58 +0900

    isconvex, GetArea, AABB

--- Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/PolyK.java
+++ Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/PolyK.java
@@ -61,9 +61,9 @@ class PolyK
 
         return p;
     }
-    boolean isSimple(int[] p)
+    boolean isSimple(float[] polygon)
     {
-        int n = p.length / 2;
+        int n = polygon.length / 2;
         if(n < 4) return true;
 
         Point a1 = _P(0, 0), a2 = _P(0, 0);
@@ -72,19 +72,19 @@ class PolyK
 
         for(int i = 0; i < n; ++i)
         {
-            a1.x = p[2*i];
-            a1.y = p[2*i + 1];
-            if(i == n-1) { a2.x = p[0]; a2.y = p[1]; }
-            else        { a2.x = p[2*i+2]; a2.y = p[2*i + 3]; }
+            a1.x = polygon[2*i];
+            a1.y = polygon[2*i + 1];
+            if(i == n-1) { a2.x = polygon[0]; a2.y = polygon[1]; }
+            else        { a2.x = polygon[2*i+2]; a2.y = polygon[2*i + 3]; }
             for(int j = 0; j < n; ++j)
             {
                 if(Math.abs(i - j) < 2) continue;
                 if(j==n-1 && j==0) continue;
 
-                b1.x = p[2*j];
-                b1.y = p[2*j+1];
-                if(j==n-1)  {b2.x = p[0]; b2.y = p[1];}
-                else        {b2.x = p[2*j+2]; b2.y = p[2*j+3];}
+                b1.x = polygon[2*j];
+                b1.y = polygon[2*j+1];
+                if(j==n-1)  {b2.x = polygon[0]; b2.y = polygon[1];}
+                else        {b2.x = polygon[2*j+2]; b2.y = polygon[2*j+3];}
 
                 if(GetLineIntersection(a1, a2, b1, b2, c) != null) return false;
             }
@@ -92,6 +92,76 @@ class PolyK
         }
 
         return true;
+    }
+    boolean isConvex(float[] polygon)
+    {
+        if(polygon.length<6) return false;
+        int l = polygon.length - 4;
+        for(int i = 0; i < l; i+=2)
+        {
+            if(!convex(polygon[i], polygon[i+1], polygon[i+2], polygon[i+3], polygon[i+4], polygon[i+5])) return false;
+        }
+        if(convex(polygon[l], polygon[l+1], polygon[l+2], polygon[l+3], polygon[0], polygon[1])) return false;
+        if(convex(polygon[l+2], polygon[l+3], polygon[0], polygon[1], polygon[2], polygon[3])) return false;
+        return true;
+    }
+    float GetArea(float[] polygon)
+    {
+        if(polygon.length <6) return 0;
+        int l = polygon.length - 2;
+        float sum = 0;
+        for(int i=0; i<l; i+=2)
+            sum += (polygon[i+2]-polygon[i]) * (polygon[i+1]+polygon[i+3]);
+        sum += (polygon[0]-polygon[l]) * (polygon[l+1]+polygon[1]);
+
+        return - sum * 0.5f;
+    }
+
+    class AABB
+    {
+        float x, y;
+        float width, height;
+        AABB(float x, float y, float width, float height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+    AABB GetAABB(float[] polygon)
+    {
+        float minx = 9999999;
+        float miny = 9999999;
+        float maxx = -minx;
+        float maxy = -miny;
+
+        for(int i = 0; i < polygon.length; i+=2)
+        {
+            minx = Math.min(minx, polygon[i]);
+            maxx = Math.max(maxx, polygon[i]);
+            miny = Math.min(miny, polygon[i+1]);
+            maxy = Math.max(maxy, polygon[i+1]);
+        }
+        return new AABB(minx, miny, maxx-minx, maxy-miny);
+    }
+
+    float[] Reverse(float[] polygon)
+    {
+        float[] np = new float[polygon.length];
+        int i = 0;
+        for(int j = polygon.length-2; j >= 0; j-=2)
+        {
+            np[i] = polygon[j];
+            np[i+1] = polygon[j+1];
+            i += 2;
+        }
+        return np;
+    }
+    
+
+    private boolean convex(float ax, float ay, float bx, float by, float cx, float cy) {
+        return (ay-by)*(cx-bx) + (bx-ax)*(cy-by) >= 0;
     }
 
     private Point GetLineIntersection(Point a1, Point a2, Point b1, Point b2, Point c) {
