41bc526b1e59d84bdc1bb79d4034cdcc74c77e79
Kim Ki Tae <celesta129@naver.com>
2020-06-19 14:59:03 +0900

    Slice 번역중. 현재 배열에 다시 배열 push하는건지 의문임.

--- Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/PolyK.java
+++ Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/PolyK.java
@@ -31,20 +31,30 @@ package kr.ac.kpu.kimkt.sliceit;
 		19. 5. 2014 - Problem with slicing fixed.
 	*/
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+class Point
+{
+    float x, y;
+    boolean flag;
+
+    static float dist(Point a, Point b)
+    {
+        float dx = b.x-a.x;
+        float dy = b.y-a.y;
+        return (float)Math.sqrt(dx*dx + dy*dy);
+    }
+
+}
 class PolyK
 {
     float x, y;
 
     boolean flag;
 
-    class Point
-    {
-        float x, y;
-        boolean flag;
-    }
+
 
     Point _P(float x, float y)
     {
@@ -93,10 +103,10 @@ class PolyK
         int l = polygon.size() - 4;
         for(int i = 0; i < l; i+=2)
         {
-            if(!convex(polygon.get(i), polygon.get(i + 1), polygon.get(i + 2), polygon.get(i + 3), polygon.get(i + 4), polygon.get(i + 5))) return false;
+            if(!Convex(polygon.get(i), polygon.get(i + 1), polygon.get(i + 2), polygon.get(i + 3), polygon.get(i + 4), polygon.get(i + 5))) return false;
         }
-        if(convex(polygon.get(l), polygon.get(l + 1), polygon.get(l + 2), polygon.get(l + 3), polygon.get(0), polygon.get(1))) return false;
-        if(convex(polygon.get(l + 2), polygon.get(l + 3), polygon.get(0), polygon.get(1), polygon.get(2), polygon.get(3))) return false;
+        if(Convex(polygon.get(l), polygon.get(l + 1), polygon.get(l + 2), polygon.get(l + 3), polygon.get(0), polygon.get(1))) return false;
+        if(Convex(polygon.get(l + 2), polygon.get(l + 3), polygon.get(0), polygon.get(1), polygon.get(2), polygon.get(3))) return false;
         return true;
     }
     float GetArea(ArrayList<Float> polygon)
@@ -173,7 +183,7 @@ class PolyK
             float cx = polygon.get(2*i2), cy = polygon.get(2*i2 + 1);
 
             boolean earFound = false;
-            if(convex(ax, ay, bx, by, cx, cy))
+            if(Convex(ax, ay, bx, by, cx, cy))
             {
                 earFound = true;
                 for(int j = 0; j < al; j++)
@@ -205,11 +215,137 @@ class PolyK
 
         return tgs;
     }
+    boolean ConstainsPoint(ArrayList<Float> polygon, float px, float py)    {
+        int n = polygon.size() / 2;
+        float ax, ay = polygon.get(2*n-3)-py, bx = polygon.get(2*n-2) - px, by = polygon.get(2*n-1) - py;
+        boolean lup = by > ay;      // left up?
+        for(int i = 0; i < n; i++)
+        {
+            ax = bx; ay = by;
+            bx = polygon.get(2*i) - px;
+            by = polygon.get(2*i + 1) - py;
+            if(ay==by) continue;
+            lup = by > ay;
+        }
+        int depth = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            ax = bx;  ay = by;
+            bx = polygon.get(2*i) - px;
+            by = polygon.get(2*i+1) - py;
+            if(ay< 0 && by< 0) continue;	// both "up" or both "down"
+            if(ay> 0 && by> 0) continue;	// both "up" or both "down"
+            if(ax< 0 && bx< 0) continue; 	// both points on the left
+
+            if(ay==by && Math.min(ax,bx)<=0) return true;
+            if(ay==by) continue;
+
+            float lx = ax + (bx-ax)*(-ay)/(by-ay);
+            if(lx == 0) return true;			// point on edge
+            if(lx > 0) depth++;
+            if(ay == 0 &&  lup && by>ay) depth--;	// hit vertex, both up
+            if(ay == 0 && !lup && by<ay) depth--; // hit vertex, both down
+            lup = by>ay;
+        }
+
+        return (depth & 1) == 1;
+    }
+    Slice(ArrayList<Float> polygon, float ax, float ay, float bx, float by)
+    {
+        if(ConstainsPoint(polygon, ax, ay) || ConstainsPoint(polygon, bx, by))
+            return [p.slice(0)];
+
+        final Point a = _P(ax, ay);
+        Point b = _P(bx, by);
+        ArrayList<Point> iscs = new ArrayList<>(); // intersections
+        ArrayList<Point> ps = new ArrayList<>();  // points
+        for(int i=0; i<polygon.size(); i+=2) ps.add(_P(polygon.get(i), polygon.get(i+1)));
+
+        for(int i=0; i<ps.size(); i++)
+        {
+            Point isc = _P(0,0);
+            isc = GetLineIntersection(a, b, ps.get(i), ps.get((i+1) % ps.size()), isc);
+            Point fisc = iscs.get(0);
+            Point lisc = iscs.get(iscs.size()-1);
+            // original : isc && .......
+            if(isc != null && (fisc== null || Point.dist(isc,fisc)>1e-10) &&
+                    (lisc==null || Point.dist(isc,lisc)>1e-10 ) )//&& (isc.x!=ps[i].x || isc.y!=ps[i].y) )
+            {
+                isc.flag = true;
+                iscs.add(isc);
+                ps.add(i+1,isc);    // original : ps.splice(i+1, 0 ,isc);
+                i++;
+            }
+
+        }
+
+        if(iscs.size() < 2) return [p.slice(0)];
+        Collections.sort(iscs, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                return (int) (Point.dist(a, p1) - Point.dist(a, p2));
+            }
+        });
+       var pgs = new ArrayList<>();
+        float dir = 0;
+        while(iscs.size() > 0)
+        {
+            int n = ps.size();
+            Point i0 = iscs.get(0);
+            Point i1 = iscs.get(1);
+
+            int ind0 = ps.indexOf(i0);
+            int ind1 = ps.indexOf(i1);
+            boolean solved = false;
+
+            if(FirstWithFlag(ps, ind0) == ind1) solved = true;
+            else
+            {
+                i0 = iscs.get(1);
+                i1 = iscs.get(0);
+                ind0 = ps.indexOf(i0);
+                ind1 = ps.indexOf(i1);
+                if(FirstWithFlag(ps, ind0) == ind1) solved = true;
+            }
+
+            if(solved)
+            {
+                dir--;
+                ArrayList<Point> pgn = GetPoints(ps, ind0, ind1);
+
+                pgs.add(pgn);
+                ps = GetPoints(ps, ind1, ind0);
+                i0.flag = i1.flag = false;
+                iscs.splice(0,2);
+                if(iscs.size() == 0) pgs.add(ps);
+            }
+
+            else { dir++; iscs.reverse(); }
+            if(dir>1) break;
+        }
+    }
+
+    private int FirstWithFlag(ArrayList<Point> ps, int ind) {
+        int n = ps.size();
+        while(true)
+        {
+            ind = (ind+1)%n;
+            if(ps.get(ind).flag ) return ind;
+        }
+    }
+
+    private ArrayList<Point> GetPoints(ArrayList<Point> ps, int ind0, int ind1) {
+        int n = ps.size();
+        ArrayList<Point> nps = new ArrayList<>();
+        if(ind1 < ind0) ind1 += n;
+        for(int i = ind0; i<= ind1; i++) nps.add(ps.get(i%n));
+        return nps;
+
+    }
 
 
-
-
-    private boolean convex(float ax, float ay, float bx, float by, float cx, float cy) {
+    private boolean Convex(float ax, float ay, float bx, float by, float cx, float cy) {
         return (ay-by)*(cx-bx) + (bx-ax)*(cy-by) >= 0;
     }
 
