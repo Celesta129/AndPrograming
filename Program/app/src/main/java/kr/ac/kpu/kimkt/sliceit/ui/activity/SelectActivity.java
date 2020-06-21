0564786568c5ab845c64ae46463c636b191aedae
Kim Ki Tae <celesta129@naver.com>
2020-06-22 06:59:23 +0900

    추가 컨텐츠 구현하다 맘..(주로 ui 표시)

---
+++ Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/ui/activity/SelectActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kpu.kimkt.sliceit.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/ui/view/GameView.java
+++ Program/app/src/main/java/kr/ac/kpu/kimkt/sliceit/ui/view/GameView.java
@@ -12,6 +12,7 @@ import android.view.Choreographer;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
